5dfa80a85eeaf9eec40ca8f5b869fccfb1d556e5
reZerOR <gkraihan14k@gmail.com>
2024-09-06 17:37:44 +0600

    navbar modified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,16 +6,26 @@ import { MenuIcon } from "lucide-react";
 
 const menu = [
   {
-    name: "Home",
-    to: "/",
+    name: "Products",
+    to: "/products",
   },
   {
-    name: 'Products',
-    to: '/products'
-  }
+    name: "Manage",
+    to: "/manage",
+  },
 ];
 
 const Navbar = () => {
+  const navlinks = menu.map((item) => (
+    <NavLink
+      to={item.to}
+      className={({ isActive }) =>
+        `${isActive ? "text-green-500" : "text-black"} font-medium`
+      }
+    >
+      {item.name}
+    </NavLink>
+  ));
   return (
     <nav className="bg-[#E3F2AC] border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -23,66 +33,25 @@ const Navbar = () => {
           to={"/"}
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <img src={logo} className="h-16" alt="plant logo" />
+          <img src={logo} className="h-10" alt="plant logo" />
         </Link>
 
+        {/* for mobile */}
         <Sheet>
           <SheetTrigger asChild>
-            <Button variant="outline" size="icon" className="lg:hidden">
-              <MenuIcon className="h-6 w-6" />
+            <Button variant="outline" size="icon" className="md:hidden">
+              <MenuIcon className="h-6 w-6 text-[#b6de24]" />
               <span className="sr-only">Toggle navigation menu</span>
             </Button>
           </SheetTrigger>
           <SheetContent side="left">
-            <div className="grid w-[200px] p-4">
-              <Link
-                to="#"
-                className="text-lg font-medium hover:underline underline-offset-4"
-              >
-                Home
-              </Link>
-              <Link
-                to="#"
-                className="text-lg font-medium hover:underline underline-offset-4"
-              >
-                About
-              </Link>
-              <Link
-                to="#"
-                className="text-lg font-medium hover:underline underline-offset-4"
-              >
-                Services
-              </Link>
-              <Link
-                to="#"
-                className="text-lg font-medium hover:underline underline-offset-4"
-              >
-                Portfolio
-              </Link>
-              <Link
-                to="#"
-                className="text-lg font-medium hover:underline underline-offset-4"
-              >
-                Contact
-              </Link>
-            </div>
+            <div className="grid w-[200px] p-4">{navlinks}</div>
           </SheetContent>
         </Sheet>
+
+        {/* for bigger screen */}
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="flex gap-6 font-medium">
-            <li>
-              <Link to={"/"}>Products</Link>
-            </li>
-            <li>
-              <Link to={"/"}>Products</Link>
-            </li>
-            <li>
-              <Link to={"/"}>Products</Link>
-            </li>
-            <li>
-              <Link to={"/"}>Products</Link>
-            </li>
-          </ul>
+          <div className="flex gap-6 font-medium">{navlinks}</div>
         </div>
       </div>
     </nav>
